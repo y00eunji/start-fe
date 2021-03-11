063f9d73f05b86fff0ebaaa57b5f8a529e52aff4
유은지 <aine0227@naver.com>
2021-03-11 18:28:14 +0900

    01 수업 과제

---
+++ 01/app.js
@@ -0,0 +1,64 @@
+console.log('app.js');
+/*
+var name ="유은지";
+var Name ="유은지";
+
+console.log(name);
+console.log(Name);*/
+
+// var var test;
+
+// var count =1;
+
+// var isMan =true;
+// var isMan = "true"; //string
+/*
+function log(str){
+    console.log(str);
+}
+log('hello');*/
+
+// var image = {
+//     width: 100,
+//     height: 100,
+//     name: '1.png',
+
+// };
+
+// var imageWidth =100;
+// var imageHeight=100;
+// var imageName ='1.png';
+
+// var image1= [1,2,5,'a',{}];
+// image1[0] = 'a';
+
+// var data =null;
+// var data1= '';
+// var data2;
+
+// console.log(data,data1, data2); -> null, " ", undefinded
+
+// console.log(1+2);
+// console.log('a'+'b');
+// console.log(1+'4');
+
+// var count =1;
+// var number ='2';
+
+// console.log( 1 ==1);
+
+// console.log('' == '0');
+// console.log(0 === '', 0 ==''); -> true, false
+// var isHero =true;
+
+// if (!isHero) {
+//     console.log('hero');
+// }
+// if (true) {
+//     console.log(1);
+// }
+// console.log(typeof 1);
+// console.log(typeof {}); //object
+// console.log(typeof []); //object
+// console.log(typeof '1');
+
