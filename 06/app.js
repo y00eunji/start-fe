6ac3ecf75b648ac6a5fb501ab94baf31cc7ca896
유은지 <aine0227@naver.com>
2021-04-10 18:48:20 +0900

    6주차 수업 내용

---
+++ 06/app.js
@@ -0,0 +1,36 @@
+// let a = 'aa';
+// const b = 'bb';
+// var c ='11';
+// c = 33;
+//
+// console.log(a,b);
+
+// var str = 'aaaaaaaaaaa\
+// ssdasd';
+// const  s = 'aaaa
+// cccccc
+// cccc'
+// console.log(str,a);
+
+// function sum(x,y=1){
+//     return x+y;
+//
+// }
+// console.log(sum(1));
+
+// const  arr = [1,2,3];
+//
+// const newArr = arr.map(item=> item + 1);
+//
+// console.log(newArr);
+//
+// const b = document.getElementById('box');
+// console.log(b);
+// b.addEventListener('click',() =>console.log(1));
+
+function sum(x,y,z){
+    return x+y+z;
+}
+const  numbers = [1,2,3];
+console.log(...numbers);
+console.log(sum(1,2,3));
