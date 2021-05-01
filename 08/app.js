ad2924788632d9c1ad71a5835fc87a7299283c63
유은지 <aine0227@naver.com>
2021-05-02 00:48:08 +0900

    08

---
+++ 08/app.js
@@ -0,0 +1,14 @@
+// const promise = fetch('http');
+// console.log(promise);
+
+// promise.then((res) => {
+//   console.log('res', res);
+//   const textPromise = res.text();
+//   console.log('textPromise', textPromise);
+
+//   textPromise.then((data) => console.log('data', data));
+// });
+
+function callback(data) {
+  console.log('callbcak:', data);
+}
