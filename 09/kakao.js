98d66581af72d80090320280d95440437f046f2b
유은지 <aine0227@naver.com>
2021-05-08 23:00:27 +0900

    09수업

---
+++ 09/kakao.js
@@ -0,0 +1,34 @@
+const $btn = document.querySelector('#search-btn');
+const $text = document.querySelector('#search-text');
+const $result = document.querySelector('#result');
+const url = 'https://dapi.kakao.com/v2/search/web';
+const headers = { Authorization: 'KakaoAK 233031c104834da88bb55fad89690735' };
+
+function success(data) {
+  const { documents } = data;
+  const li = documents.map((doc) => {
+    return `<li class="list-group-item"><a href = ${doc.url}" target="_blank">${doc.contents}</a></li>`;
+  });
+  $result.innerHTML = `<ul class="list-group list-group-flush">${li.join(
+    '',
+  )}</ul>`;
+}
+function error() {
+  alert('데이터를 가져올 수 없습니다');
+}
+function search() {
+  const { value } = $text.value;
+
+  if (value === ' ') {
+    alert('검색어를 입력하세요');
+    return false;
+  }
+
+  fetch(`${url}?query=${value}`, { headers })
+    .then((res) => res.json())
+    .then(success)
+    .catch(error);
+  return true;
+}
+
+$btn.addEventListener('click', search);
