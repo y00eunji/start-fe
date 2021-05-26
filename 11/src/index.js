16258150f7363136c13fbb265f86a81a31b20b94
유은지 <aine0227@naver.com>
2021-05-26 18:31:18 +0900

    11

---
+++ 11/src/index.js
@@ -0,0 +1,24 @@
+import './todos.css';
+
+import { render } from './html-render';
+import formInput from './form-input';
+
+const $result = document.querySelector('#result');
+
+const todos = [];
+
+$result.addEventListener('click', (event) => {
+  let { className } = event.target;
+  if (className === 'delete') {
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (className === 'toggle-checked') {
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone;
+    render(todos);
+  }
+});
+
+formInput.init(todos);
+render(todos);
